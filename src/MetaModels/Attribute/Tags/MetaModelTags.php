3ec976b42c5f0869f81251b0c023b7c7482f789a
Andreas Nölke <zero@brothers-project.de>
2015-02-13 16:19:26 +0100

    fix tab to spaces

--- src/MetaModels/Attribute/Tags/MetaModelTags.php
+++ src/MetaModels/Attribute/Tags/MetaModelTags.php
@@ -179,7 +179,7 @@ class MetaModelTags extends AbstractTags
         foreach ($items as $item) {
             $ids[] = $item->get('id');
         }
-		if (empty($ids)) {
+        if (empty($ids)) {
             return;
         }
 

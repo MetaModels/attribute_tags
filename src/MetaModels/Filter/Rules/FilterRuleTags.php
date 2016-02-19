869597a2beba08616b087e2726edb3d613bb860a
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-19 02:36:26 +0100

    Fix file headers and add license file

--- src/MetaModels/Filter/Rules/FilterRuleTags.php
+++ src/MetaModels/Filter/Rules/FilterRuleTags.php
@@ -1,11 +1,14 @@
 <?php
+
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_tags.
  *
- * PHP version 5
+ * (c) 2012-2015 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
  * @subpackage AttributeTags
@@ -15,9 +18,9 @@
  * @author     David Maack <david.maack@arcor.de>
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Martin Treml <github@r2pi.net>
- * @author  Patrick Heller <ph@wacg.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @author     Patrick Heller <ph@wacg.de>
+ * @copyright  2012-2015 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_tags/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 

809a8522bb7fce5c6e31307d3fa8fce3e7da47bd
Mini Model <minimodel@metamodel.me>
2016-03-21 00:17:23 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_attribute.php
+++ contao/languages/fr/tl_metamodel_attribute.php
@@ -8,23 +8,31 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T08:01:35+02:00
+ * last-updated: 2016-03-14T00:14:19+01:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']      = 'Paramètres d\'affichage';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']           = 'La requête SQL provoque une erreur.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']      = 'Alias de l\'étiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']      = 'Veuillez sélectionner une entrée pour l\'alias de l\'étiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']     = 'Colonne de la table';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']     = 'Veuillez sélectionner la colonne.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filter']['0']     = 'Filter';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']         = ' ID Etiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']         = 'Veuillez sélectionner une entrée pour l\'ID de l\'étiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']    = 'Tri étiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']    = 'Veuillez sélectionner une entrée pour le tri étiquette';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']      = 'Table de la base de donnée';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']      = 'Veuillez sélectionner la table de la base de donnée.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']      = 'SQL';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']      = 'La liste des options peut être limitée par l\'utilisation de SQL.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags'] = 'Etiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']                 = 'Paramètres d\'affichage';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sql_error']                      = 'La requête SQL provoque une erreur.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']                 = 'Alias de l\'étiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']                 = 'Veuillez sélectionner une entrée pour l\'alias de l\'étiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']                = 'Colonne de la table';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']                = 'Veuillez sélectionner la colonne.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column_type']['attribute']   = 'Attributs MetaModels';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column_type']['sql']         = 'Colonne de la table SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filter']['0']                = 'Filter';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filter']['1']                = 'Sélectionner un filtre à appliquer.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filterparams']['0']          = 'Paramètres de filtre.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filterparams']['1']          = 'Appliquer une valeur par défaut au filtre.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']                    = ' ID Etiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']                    = 'Veuillez sélectionner une entrée pour l\'ID de l\'étiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']               = 'Tri étiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']               = 'Veuillez sélectionner une entrée pour le tri étiquette';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']                 = 'Table de la base de donnée';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']                 = 'Veuillez sélectionner la table de la base de donnée.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['sql-table']    = 'Table SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['translated']   = 'MataModels traduits';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['untranslated'] = 'MetaModels non traduits';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']                 = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']                 = 'La liste des options peut être limitée par l\'utilisation de SQL.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags']            = 'Etiquette';
 
--- contao/languages/fr/tl_metamodel_dcasetting.php
+++ contao/languages/fr/tl_metamodel_dcasetting.php
@@ -8,11 +8,14 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-21T12:22:57+02:00
+ * last-updated: 2016-03-14T00:14:19+01:00
  */
-
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['0'] = 'Afficher comme menu';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['2'] = 'Afficher comme pop-up de sélection';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_as_wizard_reference']['3'] = 'Afficher comme liste de tags (mots-clefs)';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_maxLevel']['0']            = 'Niveau maximum dans l\'arborescence';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_maxLevel']['1']            = 'En cas de valeur >0, aucun élément ne sera sélectionnable au delà de ce niveau .';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_minLevel']['0']            = 'Niveau minimum de l\'arborescence.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['tag_minLevel']['1']            = 'En cas de valeur >0, aucun élément ne sera sélectionnable en dessous de ce niveau .';
 

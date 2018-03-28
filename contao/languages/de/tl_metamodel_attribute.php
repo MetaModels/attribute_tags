5cb204ebe8df0003e12006b97f655b3328bde77e
Mini Model <minimodel@metamodel.me>
2018-03-28 19:20:43 +0200

    Updated translations from transifex.

--- contao/languages/de/tl_metamodel_attribute.php
+++ contao/languages/de/tl_metamodel_attribute.php
@@ -8,7 +8,7 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2015-07-11T08:01:35+02:00
+ * last-updated: 2018-03-28T19:20:36+02:00
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['display_legend']                 = 'Anzeigeeinstellungen';
@@ -17,7 +17,7 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['0']                 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_alias']['1']                 = 'Bitte wählen Sie einen Eintrag für den Alias der Mehrfachauswahl aus.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['0']                = 'Tabellenspalte für Bezeichnung/Name';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column']['1']                = 'Bitte wählen Sie eine Spalte.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column_type']['attribute']   = 'MetaModels Attribute';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column_type']['attribute']   = 'MetaModels-Attribute';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_column_type']['sql']         = 'Spalte der SQL-Tabelle';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filter']['0']                = 'Filter';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filter']['1']                = 'Hier können Sie den Filter wählen.';
@@ -25,18 +25,18 @@ $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filterparams']['0']          
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_filterparams']['1']          = 'Hier können Sie eine Vorgabe für den Filter wählen.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['0']                    = 'ID der Mehrfachauswahl';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_id']['1']                    = 'Bitte wählen Sie einen Eintrag für die ID der Mehrfachauswahl aus.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']               = 'Sortierung der Mehrfachauswahl';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']               = 'Bitte wählen Sie einen Eintrag für die Sortierung der Mehrfachauswahl aus.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']                 = 'Datenbanktabelle';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']                 = 'Bitte wählen Sie eine Datenbanktabelle aus.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['sql-table']    = 'SQL Tabelle';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['translated']   = 'Übersetztes MetaModels';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['untranslated'] = 'Unübersetztes MetaModels';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']                 = 'SQL';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']                 = 'Die Auswahlmöglichkeiten können per SQL eingeschränkt werden.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags']            = 'Mehrfachauswahl';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sort']['0']                  = 'Sortierrichtung';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sort']['1']                  = 'Richtung der Sortierung.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sort_directions']['asc']     = 'Aufsteigend';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sort_directions']['desc']    = 'Absteigend';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['0']               = 'Sortierung der Mehrfachauswahl';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_sorting']['1']               = 'Bitte wählen Sie einen Eintrag für die Sortierung der Mehrfachauswahl aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['0']                 = 'Datenbanktabelle';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table']['1']                 = 'Bitte wählen Sie eine Datenbanktabelle aus.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['sql-table']    = 'SQL-Tabelle';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['translated']   = 'Übersetztes MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_table_type']['untranslated'] = 'Nicht übersetztes MetaModel';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['0']                 = 'SQL';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tag_where']['1']                 = 'Die Auswahlmöglichkeiten können per SQL eingeschränkt werden.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tags']            = 'Mehrfachauswahl [tags]';
 

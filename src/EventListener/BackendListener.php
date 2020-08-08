d588624c51a3a3e469346ebff3993918562a1d3d
zonky <info@e-spin.de>
2020-08-08 14:06:41 +0200

    Hotfix change naming in select option to new style

--- src/EventListener/BackendListener.php
+++ src/EventListener/BackendListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_tags.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
  * @author     Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_tags/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -457,7 +457,7 @@ class BackendListener
             $column = $attribute->getColName();
             $type   = $attribute->get('type');
 
-            $result[$column] = \sprintf('%s (%s - %s)', $name, $column, $type);
+            $result[$column] = \sprintf('%s [%s, "%s"]', $name, $type, $column);
         }
 
         return $result;

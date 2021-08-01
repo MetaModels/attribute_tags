97e9010d9d393d248e21e19d963f1c49b59912cb
Ingolf Steinhardt <info@e-spin.de>
2021-08-01 19:26:33 +0200

    Switch to github-actions

--- tests/Attribute/MetaModelTagsTest.php
+++ tests/Attribute/MetaModelTagsTest.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_tags.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2021 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels/attribute_tags
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2021 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_tags/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -32,6 +33,8 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * Unit tests to test class MetaModelTags.
+ *
+ * @covers \MetaModels\AttributeTagsBundle\Attribute\MetaModelTags
  */
 class MetaModelTagsTest extends TestCase
 {

22b4950f567bf8aadfa2d01d292d454e3696f014
Ingolf Steinhardt <git@e-spin.de>
2022-01-28 23:37:42 +0100

    adapt_language_key_with_country_code

--- src/Attribute/MetaModelTags.php
+++ src/Attribute/MetaModelTags.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_tags.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_tags/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -449,7 +449,7 @@ class MetaModelTags extends AbstractTags
         $metaModel = $this->getTagMetaModel();
         if (!$metaModel instanceof ITranslatedMetaModel) {
             $originalLanguage       = $GLOBALS['TL_LANGUAGE'];
-            $GLOBALS['TL_LANGUAGE'] = $this->getMetaModel()->getActiveLanguage();
+            $GLOBALS['TL_LANGUAGE'] = \str_replace('_', '-', $this->getMetaModel()->getActiveLanguage());
         }
 
         $filter = $this->getTagMetaModel()->getEmptyFilter();
